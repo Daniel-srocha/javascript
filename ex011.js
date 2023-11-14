69a790e493e76ac2f6119f96b4c855838b1f1ed6
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-14 15:15:48 -0300

    condicionais

---
+++ ex011.js
@@ -0,0 +1,48 @@
+// Condicionais
+
+// IF
+
+let ligado = false
+
+if(ligado){
+    console.log("Executou comando")
+}
+
+let ovos = false
+let itens = "leite"
+if(ovos){
+  itens = "leite"
+}
+
+console.log("item comprado: " + itens)
+
+
+
+// ELSE
+
+
+
+let ovos1 = false
+let item = "Leite"
+if(ovos){
+  
+}
+else{
+    console.log("item comprado: " + item)
+}
+
+
+
+// else if
+
+let nivel = 1
+
+if(nivel === 1){
+    console.log("pouco")
+}else if(nivel === 2){
+    console.log("Quase")
+}else{
+    console.log("Certo")
+}
+
+
