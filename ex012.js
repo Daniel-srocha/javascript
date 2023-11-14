f2510ffdfbdfeb827b8ed3b83b631a2c1302b979
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-14 15:41:47 -0300

    Switch case

---
+++ ex012.js
@@ -0,0 +1,19 @@
+// Switch-Case
+
+
+
+let fruta = "banana"
+
+switch (fruta){
+    case "laranja":
+        console.log("Suco de laranja")
+        break
+    case "banana":
+        console.log("Vitamina de banana")
+        break
+    case "maçã":
+        console.log("suco de maçã")
+        break
+    default:
+        console.log("suco genérico")
+}
