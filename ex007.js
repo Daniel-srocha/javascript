4766d7762574f73e8edc86703e68bf6085eb4e81
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-14 12:25:47 -0300

    operadores

---
+++ ex007.js
@@ -0,0 +1,42 @@
+// operadores e expressões
+
+// Operadores matemáticos
+
+let idade = 30
+console.log(idade)
+
+idade = 30 + 6
+console.log(idade)
+
+idade = 30 - 6
+console.log(idade)
+
+idade = 30 * 6
+console.log(idade)
+
+idade = 30 / 6
+console.log(idade)
+
+idade = 30 % 6
+console.log(idade)
+
+idade = 30 ** 6
+console.log(idade)
+
+// incremento decremento
+
+let contador = 1
+contador++
+console.log(contador)
+contador--
+console.log(contador)
+
+// Atribuição
+
+let preco = 10
+preco += 5
+preco -= 5
+
+
+
+
