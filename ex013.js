3cddb373ba76e404a2e76dd6db8a9d04e439aa45
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-14 16:01:19 -0300

    Laços de repetição

---
+++ ex013.js
@@ -0,0 +1,35 @@
+// Laços de repetição
+
+
+// FOR
+
+for (let contador = 0; contador < 4; contador++){
+    console.log(contador)
+    console.log("Aumentando contador")
+}
+
+let pontos = 0
+for(let i = 0; i < 11; i++){
+    pontos += 1
+    console.log("aumentou os pontos em " + i)
+}
+
+console.log(pontos + " Total")
+
+
+// while
+
+let i = 0
+while(i < 3){
+    console.log("Olá")
+    i++
+}
+
+
+// do while
+
+let p = 0
+do {
+    console.log("olá")
+    p++
+} while (p < 3)
